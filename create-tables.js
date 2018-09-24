efdf05da6aae1d1560c43594d4493ec2d07f8f74
esnosek <esnosek@gmail.com>
2018-09-24 16:25:19 +0200

    create db tables

--- create-tables.js
+++ create-tables.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
 });
 
 async function createClientTable(){
-    var sql = "CREATE TABLE client (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), photo VARCHAR(255), description VARCHAR(255))";
+    var sql = "CREATE TABLE client (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), photo VARCHAR(255), description TEXT)";
     connection.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Table \'client\' created");
@@ -16,15 +16,27 @@ async function createClientTable(){
 }
 
 async function createOrderTable(){
-  
+    var sql = "CREATE TABLE order (id INT AUTO_INCREMENT PRIMARY KEY, client_id INT, type_id INT, quantity INT, description TEXT";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table \'order\' created");
+    });  
 }
 
-async function createAppleTypeTable(){
-  
+async function createTypeTable(){
+    var sql = "CREATE TABLE type (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), photo VARCHAR(255), dafault_price INT, description TEXT)";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table \'type\' created");
+    });
 }
 
 async function createPantryTable(){
-  
+    var sql = "CREATE TABLE pantry (id INT AUTO_INCREMENT PRIMARY KEY, type_id INT, quantity INT)";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table \'pantry\' created");
+    });  
 }
 
 createClientTable()
